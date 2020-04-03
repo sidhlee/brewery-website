2609b2c6536ee108d3d6d6df6e0493ebe59e4544
Hayoun Lee <sidhlee@gmail.com>
2020-04-03 13:57:01 +0900

    Fix bug in sliding functions + refactor

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,15 +1,56 @@
-/* TODO: fix error in carousel OR redo  */
-
 const carousel = document.querySelector(".grid-carousel");
+const slideContainer = document.querySelector(".slides-container");
 const slides = carousel.querySelectorAll(".grid-carousel__item");
 const leftButton = carousel.querySelector(".js-left");
 const rightButton = carousel.querySelector(".js-right");
 
+rightButton.addEventListener("click", () => slideAndReOrder(true));
+leftButton.addEventListener("click", () => slideAndReOrder(false));
+
+function slideAndReOrder(right = true) {
+  if (right) {
+    carousel.addEventListener("animationend", slideRight);
+  } else {
+    carousel.addEventListener("animationend", slideLeft);
+  }
+  animateSlide(right);
+}
+
+function animateSlide(right = true) {
+  slideContainer.style.animation = `slide-${
+    right ? "right" : "left"
+  } 500ms ease-in-out`;
+}
+
+// slide right and cleanup
+function slideRight() {
+  slide();
+  carousel.removeEventListener("animationend", slideRight);
+  // remove animation value so that it can be re-added to initiate animation
+  slideContainer.style.animation = "";
+}
+
+// slide left and cleanup
+function slideLeft() {
+  slide(false);
+  carousel.removeEventListener("animationend", slideLeft);
+  slideContainer.style.animation = "";
+}
+
+// slide all element to the right(default) or left
+function slide(right = true) {
+  const offsetBy = right ? 1 : -1;
+  reOrderAll(offsetBy);
+}
+
+// get CSS order property value for given element
 function getOrder(elem) {
   const styles = getComputedStyle(elem);
   return +styles.order; // computedStyle has all the values as string
 }
 
+// Re-order all elements in slides node by the given offset-value
+// NOTE: CSS order starts from 1
 function reOrderAll(offsetBy = 1) {
   slides.forEach(slide => {
     const currOrder = getOrder(slide);
@@ -25,52 +66,5 @@ function reOrderAll(offsetBy = 1) {
   });
 }
 
-// init order (IPA in the middle!);
+// init order (place IPA in the middle);
 reOrderAll(2);
-
-// to the right => decrement order
-rightButton.addEventListener("click", () => slideAndReOrder(true));
-leftButton.addEventListener("click", () => slideAndReOrder(false));
-
-function slide(right = true) {
-  slides.forEach(slide => {
-    slide.style.animation = `slide-${
-      right ? "right" : "left"
-    } 750ms ease-in-out`;
-  });
-}
-
-function slideAndReOrder(right = true) {
-  if (right) {
-    carousel.addEventListener("animationend", slideRight);
-  } else {
-    carousel.addEventListener("animationend", slideLeft);
-  }
-
-  slide(right);
-}
-
-function slideRight(e) {
-  reOrder(e, true);
-  carousel.removeEventListener("animationend", slideRight);
-}
-
-function slideLeft(e) {
-  reOrder(e, false);
-  carousel.removeEventListener("animationend", slideLeft);
-}
-
-function reOrder(e, right) {
-  const offsetBy = right ? -1 : 1;
-  console.log(offsetBy);
-  const slide = e.target;
-  const currOrder = slide.style.order;
-  e.target.style.order =
-    ((currOrder - 1 + offsetBy + slides.length) % slides.length) + 1;
-  if (slide.style.order === "3") {
-    // DOM style props are strings!
-    slide.classList.add("featured");
-  } else {
-    slide.classList.remove("featured");
-  }
-}
